be2ca94789e786cc9edac370c4d7b37edd57c3f7
linwCoder <770601773@qq.com>
2016-08-19 20:02:32 +0800

    使用storyboard获取默认的控制器

---
+++ 19-创建控制器/19-创建控制器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  19-创建控制器
+//
+//  Created by 林威 on 16/8/19.
+//  Copyright © 2016年 linw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 19-创建控制器/19-创建控制器/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  19-创建控制器
+//
+//  Created by 林威 on 16/8/19.
+//  Copyright © 2016年 linw. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
